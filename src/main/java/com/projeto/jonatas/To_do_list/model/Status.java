f6258e84262e818304b4813b691e42b8ed585753
jonatas <jonatas_dev@hotmail.com>
2025-09-07 17:56:23 -0300

    Inicial project

---
+++ src/main/java/com/projeto/jonatas/To_do_list/model/Status.java
@@ -0,0 +1,6 @@
+package com.projeto.jonatas.To_do_list.model;
+
+public enum Status {
+    PENDENTE,
+    CONCLUIDO
+}
---
+++ src/main/java/com/projeto/jonatas/To_do_list/model/TaskModel.java
@@ -0,0 +1,28 @@
+package com.projeto.jonatas.To_do_list.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "task")
+@Data
+public class TaskModel {
+
+   @Id
+   @Column(name = "id")
+   @GeneratedValue
+   private UUID id;
+
+   @Column(name = "title")
+   private String title;
+
+   @Column(name = "description")
+   private String descriptiom;
+
+   @Column(name = "status")
+   private Status status;
+
+
+}
